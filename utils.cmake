f2aa34a55ca462bc8a374f7bf7450a466ff88a55
Zottel0r <zottel0r@gmail.com>
2025-03-07 12:09:22 +0100

    move string related functions to stringTools

--- utils.cmake
+++ utils.cmake
@@ -1,66 +1,4 @@
-# set(bar 1 2 3 4 )
-
-# print(bar NUM -2 GLUE "." BREAK)
-# >> bar:
-# .3
-# .4
-
-# print(CMAKE_CURRENT_SOURCE_DIR)
-
-# >> CMAKE_CURRENT_SOURCE_DIR = /home/foo/work/fobi/2023-02-02-KDAB-CMake/training-handout/cmake/lab-codegenerator
-
-# print(bar BREAK)
-# >> bar:
-# ---- 1
-# ---- 2
-# ---- 3
-# ---- 4
-
-# print(bar GLUE ".")
-# >> bar: 1.2.3.4
-function(___)
-endfunction()
-
-#
-# Usage:
-# str2Color(<color> <variable> [REVERSE])
-# sourrounds a string with escape sequences. Example:
-#
-# Example:
-# set(foo "Wow my text")
-# str2Color(Red foo)
-# # then foo will be: "\x1b[31mWow my Text\x1b[m"
-# - REVERSE: will first put the color reset, then the color
-# # then foo will be: "\x1b[mWow my Text\x1b[31m")
-function(str2color s2c_type var)
-    if(NOT WIN32)
-        string(ASCII 27 Esc)
-        set(ColourReset "${Esc}[m")
-        set(ColourBold "${Esc}[1m")
-        set(Red "${Esc}[31m")
-        set(Green "${Esc}[32m")
-        set(Yellow "${Esc}[33m")
-        set(Blue "${Esc}[34m")
-        set(Magenta "${Esc}[35m")
-        set(Cyan "${Esc}[36m")
-        set(White "${Esc}[37m")
-        set(BoldRed "${Esc}[1;31m")
-        set(BoldGreen "${Esc}[1;32m")
-        set(BoldYellow "${Esc}[1;33m")
-        set(BoldBlue "${Esc}[1;34m")
-        set(BoldMagenta "${Esc}[1;35m")
-        set(BoldCyan "${Esc}[1;36m")
-        set(BoldWhite "${Esc}[1;37m")
-    endif()
-
-    if(DEFINED ${s2c_type})
-        if(ARGN STREQUAL REVERSE)
-            set(${var} ${ColourReset}${${var}}${${s2c_type}} PARENT_SCOPE)
-        else()
-            set(${var} ${${s2c_type}}${${var}}${ColourReset} PARENT_SCOPE)
-        endif()
-    endif()
-endfunction()
+include(${CMAKE_CURRENT_LIST_DIR}/stringTools.cmake)
 
 #
 # Usage:
@@ -68,25 +6,30 @@ endfunction()
 # outputs a colored Message according to:
 # - HIGH: Magenta
 # - NOTE: Cyan
-# - OK: Green
-# - ERR: BoldRed
-# - CRIT: BoldYellow
+# - OK:   Green
+# - WARN: BoldYellow
+# - ERR:  Red
+# - CRIT: BoldRed
 function(msg msg_type msg_var)
     if(${msg_type} STREQUAL HIGH)
-        str2color(Magenta msg_var)
-        message("## ${msg_var}")
+#        str2color(Magenta msg_var)
+        str2color(COLOR magenta msg_var)
+        message(" ## ${msg_var} ")
     elseif(${msg_type} STREQUAL NOTE)
-        str2color(Cyan msg_var)
-        message("## ${msg_var}")
+        str2color(COLOR Cyan msg_var)
+        message(" ## ${msg_var} ")
     elseif(${msg_type} STREQUAL OK)
-        str2color(Green msg_var)
-        message("## ${msg_var}")
+        str2color(COLOR Green msg_var)
+        message(" ## ${msg_var} ")
     elseif(${msg_type} STREQUAL ERR)
-        str2color(BoldRed msg_var)
-        message("## ${msg_var}")
+        str2color(COLOR Red msg_var)
+        message(" ## ${msg_var} ")
     elseif(${msg_type} STREQUAL CRIT)
-        str2color(BoldYellow msg_var)
-        message("## ${msg_var}")
+        str2color(BOLD COLOR Red msg_var)
+        message(" ## ${msg_var} ")
+    elseif(${msg_type} STREQUAL WARN)
+        str2color(BOLD COLOR Yellow msg_var)
+        message(" ## ${msg_var} ")
     else()
         message(${msg_type} ${msg_var})
     endif()
@@ -102,9 +45,12 @@ function(printVar)
 
     set(var_name ${ARGV0})
     set(var_value ${${ARGV0}})
-    str2color(Bold${PRINT_COLOR} var_name)
-    str2color(${PRINT_COLOR} var_value)
-    message(">> ${var_name} = ${var_value}")
+#    if(DEFINED PRINT_COLOR)
+##        set(PRINT_COLOR White)
+        str2color(BOLD COLOR ${PRINT_COLOR} var_name)
+        str2color(COLOR ${PRINT_COLOR} var_value)
+#    endif()
+    message(">> ${var_name} = ${var_value} ")
 endfunction()
 
 #
@@ -113,11 +59,15 @@ endfunction()
 # print(<list> [NUM <num_values_shown>] [GLUE <glue_string>] [BREAK])
 function(print)
     set(options BREAK)
-    set(oneValueArgs NUM GLUE)
+    set(oneValueArgs NUM GLUE COLOR)
     set(multiValueArgs)
     cmake_parse_arguments(p "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     list(LENGTH ${ARGV0} len)
+
+    if(p_COLOR)
+        set(PRINT_COLOR ${p_COLOR})
+    endif()
 
     if(len GREATER 1)
         printList(${ARGV0} ${ARGN})
@@ -173,12 +123,19 @@ function(printList liste)
         math(EXPR start "${len} + ${p_NUM}" OUTPUT_FORMAT DECIMAL)
     endif()
 
-    list(SUBLIST ${liste} ${start} ${stop} value)
-    str2color(${PRINT_COLOR} glue REVERSE)
-    list(JOIN value "${glue}" value)
-    str2color(Bold${PRINT_COLOR} name)
-    str2color(${PRINT_COLOR} sep REVERSE)
-    message("${name}${sep}${value}")
+    list(SUBLIST ${liste} ${start} ${stop} liste)
+
+    str2color(BOLD COLOR ${PRINT_COLOR} name)
+
+    list(GET liste 0 result)
+    str2color(COLOR ${PRINT_COLOR} result)
+
+    list(SUBLIST liste 1 ${stop} liste)
+    foreach(v ${liste})
+        str2color(COLOR ${PRINT_COLOR} v)
+        set(result "${result}${glue}${v}")
+    endforeach()
+    message(">> ${name}${sep}${result}")
 endfunction()
 
 #
@@ -196,8 +153,17 @@ function(dump_variables)
     # message("--------------------")
     # set(arg0 ${ARGV0})
     # set(arg1 ${ARGV1})
-    list(POP_FRONT p_UNPARSED_ARGUMENTS regex)
-    list(POP_FRONT p_UNPARSED_ARGUMENTS exclude)
+
+    #Compatability to CMake 3.14 (Yocto)
+    # list(POP_FRONT p_UNPARSED_ARGUMENTS regex)
+    # list(POP_FRONT p_UNPARSED_ARGUMENTS exclude)
+    list(LENGTH p_UNPARSED_ARGUMENTS length)
+    if(length GREATER 0)
+        list(GET p_UNPARSED_ARGUMENTS 0 regex)
+    endif ()
+    if(length GREATER 1)
+        list(GET p_UNPARSED_ARGUMENTS 1 exclude)
+    endif ()
 
     # set(regex ${regex})
     # print(arg0)
@@ -207,11 +173,10 @@ function(dump_variables)
     # print(exclude)
     # print(p_BREAK)
     # set(printArgs NUM ${p_NUM} GLUE ${p_GLUE} ${p_BREAK})
-    message("--------------------")
+    message(" -------------------- ")
 
     get_cmake_property(_variableNames VARIABLES)
     list(SORT _variableNames)
-
     foreach(_variableName ${_variableNames})
         if(regex)
             unset(MATCHED)
@@ -230,9 +195,42 @@ function(dump_variables)
                 continue()
             endif()
         endif()
+        set(matchFound TRUE)
 
         print(${_variableName} ${ARGN})
     endforeach()
+#    list(LENGTH _variableNames _entries)
+#    print(_entries)
+    if(NOT matchFound)
+        set(notFound  NOT_FOUND )
+        str2color(COLOR WHITE regex)
+        str2color(COLOR Yellow notFound)
+        #    endif()
+        message(">> ${regex} = ${notFound} ")
+#        msg(WARN ">> ${regex} NOT_FOUND ")
+    endif()
 
-    message("--------------------")
+    message(" -------------------- ")
 endfunction()
+
+
+# set(bar 1 2 3 4 )
+
+# print(bar NUM -2 GLUE "." BREAK)
+# >> bar:
+# .3
+# .4
+
+# print(CMAKE_CURRENT_SOURCE_DIR)
+
+# >> CMAKE_CURRENT_SOURCE_DIR = /home/foo/work/fobi/2023-02-02-KDAB-CMake/training-handout/cmake/lab-codegenerator
+
+# print(bar BREAK)
+# >> bar:
+# ---- 1
+# ---- 2
+# ---- 3
+# ---- 4
+
+# print(bar GLUE ".")
+# >> bar: 1.2.3.4
